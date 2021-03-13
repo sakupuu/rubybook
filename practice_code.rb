6b1bfa5d15c751f589f31e160d8ec3e33df6b13a
sakupuu <saku.yuzu.113@gmail.com>
2021-03-13 11:51:48 +0900

    練習問題を付け加えました

--- practice_code.rb
+++ practice_code.rb
@@ -115,3 +115,80 @@
 #   n += 1
 # end
 # puts(sum)
+
+#楽しいRubyp.262練習問題
+#1.摂氏温度を華氏温度に変換するcels_to_fahrを定義せよ。摂氏温度と華氏温度の変換は以下のようになる。
+#華氏＝摂氏×９÷５＋３２
+# def cels_to_fahr(c)
+#   f = c*9/5+32
+#   puts "#{c} degree Celsius = #{f.to_i} degree Fahrenheit."
+# end
+#解答例
+# def cels2fahr(cels)
+#   return cels * 9.0 / 5.0 + 32.0
+# end
+# 解説：９と５はそれぞれ9.0や5.0にしています。こうしないと、celsが整数だった場合、正しく計算されません(5で割るところの結果がIntegerになってしまいます)
+  
+
+#2. 1.とは逆に華氏温度を摂氏温度に変換するメソッドfahrs_to_cellを定義せよ。
+#また、摂氏一度から摂氏１００度まで一度刻みに華氏温度との対応を出力せよ。
+# def fahr_to_cells(f)
+#   c = (f-32) * 5 / 9
+#   puts "#{f} degree Fahrenheit is #{c.to_i} degree Celsius."
+#   return c.to_i
+#   end
+  
+#   1.upto(100) do |i|
+#     cels_to_fahr(i)
+#   end
+
+#解答例
+# def fahr2cels(fahr)
+#   return (fahr.to_f - 32) * 5.0 / 9.0
+# end
+#解説：(1)の逆の計算を定義します。 「+」と「*」の優先順位の違いから括弧が必要なことに注意してください。
+# 1.upto(100) do |i|
+#   print i, " ", fahr2cels(i), "\n"
+# end
+#解説：華氏を1度から100度まで変えるには、uptoメソッドを使います。
+
+#3.サイコロを振って出た目（１〜６までのランダムな整数）を返すメソッドdiceを定義せよ。
+# def dice(rand)
+# end
+# puts rand(1..6)
+#解答例
+# def dice
+#   return Random.rand(6) + 1
+# end
+#解説：単に「Random.rand(6)」とした場合は、0から5の値が返るので結果に1を足します。
+
+#4.１０個のサイコロを振って出た目の合計を返すメソッドdice10を定義せよ。
+#解答例
+# def dice
+#   return Random.rand(6) + 1
+# end
+
+# def dice10
+#   ret = 0
+#   10.times do
+#     ret += dice
+#   end
+#   ret
+# end
+#解説：単純にdice+dice+...と書いても可能ではありますが、やはりここは繰り返しを使うべきでしょう。
+
+#整数numが素数であるかどうかを調べるprime?(num)を定義せよ。なお、素数とは「それ自身と１位外で割り切ることのできない数」です。一桁の整数のうち、素数となるのは「２、５、３、７」です。
+# def prime?(num)
+#   return false if num < 2
+#   2.upto(Math.sqrt(num)) do |i|
+#     if num % i == 0
+#       return false
+#     end
+#   end
+#   return true
+# end
+
+# 1.upto(10) do |n|
+#   puts n if prime?(n)
+# end
+#解答例：まず、2より小さい数は素数でないとします。それ以上の数は、2からその数の平方根まで割り算を行って、すべての剰余が0でないことを確認します。
