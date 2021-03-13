fdc61ab95993998b483fecd0ed954685ee0febbc
sakupuu <saku.yuzu.113@gmail.com>
2021-03-13 21:50:45 +0900

    p.324練習問題を追加しました

--- practice_code.rb
+++ practice_code.rb
@@ -307,3 +307,89 @@
 # end
 
 # p sum_array([1, 2, 3], [4, 6, 8])
+
+#p.324 練習問題
+#1. "Ruby is an object oriented programming language"という文字列があります。この文字列に含まれる各単語を要素とする並列を作りなさい。
+#私の回答
+# str = "Ruby is an object oriented programming language"
+# p str [0..46]
+#解答例
+# str = "Ruby is an object oriented programming language"
+# ary = str.split
+# p ary
+
+#2. 1.の配列をアルファベット順にソートせよ。
+#私の回答
+# a = [Ruby is an object oriented programming language]
+# a.sort!
+# p a 
+#解答例
+#アルファベットだけの場合、引数なしのArray#sortでソートされます。この辺りは配列の復習にもなっています。
+# str = "Ruby is an object oriented programming language"
+# ary = str.split
+# p ary.sort
+
+#3. 2.の配列を大文字と小文字の区別をせずにアルファベット順にソートせよ。
+#私の回答
+#str = "Ruby is an object oriented programming language"
+#解答例
+#こちらではArray#sort_byを使って、引数の比較をしています。その際、String#downcaseで強制的に小文字にしてから比較するため、大文字小文字の区別がなくなります。
+# str = "Ruby is an object oriented programming language"
+# ary = str.split
+# p ary.sort_by{|s| s.downcase }
+
+#5. 1.の文字列に含まれる文字とその数を次のような形式で表示させよ。（空白文字が6つ、'R'が1つ、'a'が4つ、'b'が・・・・という意味です）
+#解答例
+#ハッシュを作成して、文字をキー、出現した回数を値として記録します。最後に文字をソートして回数個数のアスタリスクとともに出力します。
+# str = "Ruby is an object oriented programming language"
+# count = Hash.new
+# str.each_char do |c|
+#   count[c] = 0 unless count[c]
+#   count[c] += 1
+# end
+# count.keys.sort.each do |c|
+#   printf("'%s': %s\n", c, "*" * count[c])
+# end
+
+#6. "七千百二十三"といった、漢数字による1~9999の数の表現を、「7213」のような数値に変換するメソッドkan2numを定義してください。
+#解答例
+#この問題は応用問題でかなり難しいです。
+#各桁の数字をString#gsub!で置き換えていきます。桁の切り出しには正規表現を駆使しています。
+# def kan2num(string)
+#   digit4 = digit3 = digit2 = digit1 = "0"
+
+#   nstring = string.dup
+#   nstring.gsub!(/一/, "1")
+#   nstring.gsub!(/二/, "2")
+#   nstring.gsub!(/三/, "3")
+#   nstring.gsub!(/四/, "4")
+#   nstring.gsub!(/五/, "5")
+#   nstring.gsub!(/六/, "6")
+#   nstring.gsub!(/七/, "7")
+#   nstring.gsub!(/八/, "8")
+#   nstring.gsub!(/九/, "9")
+
+#   if nstring =~ /((\d)?千)?((\d)?百)?((\d)?十)?(\d)?$/
+#     if $1
+#       digit4 = $2 || "1"
+#     end
+#     if $3
+#       digit3 = $4 || "1"
+#     end
+#     if $5
+#       digit2 = $6 || "1"
+#     end
+#     digit1 = $7 || "0"
+#   end
+
+#   return (digit4+digit3+digit2+digit1).to_i
+# end
+
+# p kan2num("七千八百二十三")
+# p kan2num("千八百二十三")
+# p kan2num("八百二十三")
+# p kan2num("百二十三")
+# p kan2num("百三")
+# p kan2num("二十三")
+# p kan2num("十三")
+# p kan2num("三")
