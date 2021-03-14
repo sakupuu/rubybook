646e1a5881088d44d0fe82fad1f8625b3892971d
sakupuu <saku.yuzu.113@gmail.com>
2021-03-14 18:28:29 +0900

    times追加しました

--- challenge.rb
+++ challenge.rb
@@ -17,14 +17,34 @@
 # first, second, third = 1, 2, 3
 # puts [first, second, third]
 
-cats = ["ぷう", "ゆず", "ライチ"]
-cats.each do |cat|
-  case cat
-  when ["ぷう"]
-    puts "#{cat}はよく食べて元気な天真爛漫娘です"
-  when ["ゆず"]
-    puts "#{cat}は気難しいですがとっても優しい男の子です"
-  else ["ライチ"]
-    puts "#{cat}は人懐っこくておおらかな性格の最年長です"
-  end
-end
+# cats = ["ぷう", "ゆず", "ライチ"]
+# cats.each do |cat|
+#   case cat
+#   when ["ぷう"]
+#     puts "#{cat}はよく食べて元気な天真爛漫娘です"
+#   when ["ゆず"]
+#     puts "#{cat}は気難しいですがとっても優しい男の子です"
+#   else ["ライチ"]
+#     puts "#{cat}は人懐っこくておおらかな性格の最年長です"
+#   end
+# end
+
+# name = "inoue"
+# puts name.reverse
+# puts name.length
+# puts name.upcase
+
+# 5.times do
+#   puts "私はぷうです"
+# end
+
+# 10.times do |i| 
+#   puts "#{i+1}回目の繰り返しです"
+# end
+
+# sum = 0
+# for i in 1..5
+#   sum += i
+# end
+# puts sum
+
